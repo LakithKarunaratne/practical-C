96e3f8035f3d2ff1bc85e679de59edf951e189ce
Lakith <lakith.hos@outlook.com>
2019-01-16 15:38:40 +0530

    Create define-structures.c

---
+++ src/define-structures.c
@@ -0,0 +1,14 @@
+// ------ Define Structures --------
+// compiled using the preprocessor
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(){
+	printf ("Value of true: %d\n", TRUE);
+	printf ("Value of false: %d\n", FALSE);
+	
+	return 0;
+}
