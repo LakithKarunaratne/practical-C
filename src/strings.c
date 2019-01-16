42e63cdf01b650a06d696282f73c76baefe50ad7
Lakith <lakith.hos@outlook.com>
2019-01-16 15:37:49 +0530

    Create strings.c

---
+++ src/strings.c
@@ -0,0 +1,9 @@
+// In C strings are represented by the pointer type char*. Under the hood they are stored as a list of characters, where the final character is a special character called the null terminator. Strings are a complicated and important part of C, which we'll learn to use effectively in the next few chapters.
+
+// Strings can also be declared literally by putting text between quotation marks. We used this in the previous chapter with our string "Hello, World!". For now, remember that if you see char*, you can read it as a string.
+
+// char* is a string
+// strings are stored as list of characters 
+
+printf("Your grade is %c\n", grade);
+printf("value of a: %d\n", a);
