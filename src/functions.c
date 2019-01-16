491d3d2fc26d3f36764c98bcac2427e45d3e5f64
Lakith <lakith.hos@outlook.com>
2019-01-16 15:36:59 +0530

    Create functions.c

---
+++ src/functions.c
@@ -0,0 +1,18 @@
+// int add_together(int x, int y){
+// 	int result = x + y;
+// 	return result;
+// }
+// // need to be inside a main ? or function ?
+// int added = add_together(10, 12);
+
+// // ------- TODO: How to get the output ? -------
+// int main () {
+//    /* local variable definition */
+//    int a = 10;
+//    /* do loop execution */
+//    do {
+//       printf("value of a: %d\n", a); // <--- this is how you include in the variable to the output 
+//       a = a + 1;
+//    }while( a < 20 );
+//    return 0;
+// }
